f4f15038632c1d1a400c8444979f85323083988a
rommoi <raman.maiseichuk@gmail.com>
2019-03-31 21:47:55 +0300

    stringConvertion

--- Practice.cs
+++ Practice.cs
@@ -34,9 +34,30 @@ namespace ConsoleApp7
 
             //2. STRING CONVERSION           
             string hasPhotoString = "True";//to bolean
+
+            //bool hasPhotoString_bool = hasPhotoString;                        //Implicit. Fail. Not Compiling
+            //bool hasPhotoString_bool1 = (bool)hasPhotoString;                 //Explicit. Fail. Not Compiling
+            bool hasPhotoString_bool2 = Convert.ToBoolean(hasPhotoString);    //Convert. OK.
+
             string flatNumber = "34";//to int
+
+            //int flatNumber_int = flatNumber;                        //Implicit. Fail. Not Compiling
+            //int flatNumber_int1 = (int)flatNumber;                  //Explicit. Fail. Not Compiling
+            int flatNumber_int2 = Convert.ToInt32(flatNumber);      //Convert. OK.
+
             string visaPriceString = "34,23";//to float
+
+            //float visaPriceString_float = visaPriceString;                             //Implicit. Fail. Not Compiling
+            //float visaPriceString_float1 = (float)visaPriceString;                     //Explicit. Fail. Not Compiling
+            //float visaPriceString_float2 = Convert.ToDouble(visaPriceString);          //Convert.ToDouble(). Fail. Not compiling
+            float visaPriceString_float3 = (float)Convert.ToDouble(visaPriceString);   //Explicit. (float)Convert.ToDouble(). Fail. result (float)3423
+
             string photoPriceString = "7.23";//to float
+
+            //float photoPriceString_float = photoPriceString;                            //Implicit. Fail. Not Compiling
+            //float photoPriceString_float1 = (float)photoPriceString;                    //Explicit. Fail. Not Compiling
+            //float photoPriceString_float2 = Convert.ToDouble(photoPriceString);         //Convert.ToDouble(). Fail. Not Compiling
+            float photoPriceString_float3 = (float)Convert.ToDouble(photoPriceString);  //Explicit. (float)Convert.ToDouble(). OK. result 7.23
 
             //3. BOOL CONVERSION
             bool hasFree2Pages = false;//to string, to char, to int
