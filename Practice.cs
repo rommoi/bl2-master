744c7c6da44d18b6794876a188fc31bb2b24b2af
rommoi <raman.maiseichuk@gmail.com>
2019-03-31 21:19:51 +0300

    charChanges

--- Practice.cs
+++ Practice.cs
@@ -14,10 +14,23 @@ namespace ConsoleApp7
 
             //1. CHAR CONVERSION 
             char sex = 'М';//to string
-            string sex1 = Convert.ToString(sex); //"M"
+
+            //string sex_str = sex;                  //Implicit. Fail. Not compiling..
+            //string sex_str2 = (string)sex;         //Explicit. Fail. Not compiling..
+            string sex_str3 = Convert.ToString(sex); //Convert. OK..
+            string sex_str4 = sex.ToString();        //ToString(). OK.
 
             char place = '3';//to int
+
+            int place_int = place;                   //Implicit. 
+            int place_int1 = (int)place;             //Explicit.
+            int place_int2 = Convert.ToInt32(place); //Convert.
+
             char hasFingerPrints = '0';//to boolean
+
+            //bool hasFingerPrints_bool = hasFingerPrints;                      //Implicit. Fail. Not Compiling
+            //bool hasFingerPrints_bool1 = (bool)hasFingerPrints;               //Explicit. Fail. Not Compiling
+            //bool hasFingerPrints_bool2 = Convert.ToBoolean(hasFingerPrints);  //Convert. Fail. System.InvalidCastException
 
             //2. STRING CONVERSION           
             string hasPhotoString = "True";//to bolean
